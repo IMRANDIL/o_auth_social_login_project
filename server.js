89550fe45e6725e950573e20b48abf419d1211ee
IMRANDIL <aliimranadil2@gmail.com>
2022-08-07 12:28:58 +0530

    yh

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.URI).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}😃`);
+  });
+});
