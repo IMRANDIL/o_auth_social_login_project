a0afeae780516ca156eaf1e36e7e23cd2c2313d0
IMRANDIL <aliimranadil2@gmail.com>
2022-08-07 16:55:27 +0530

    yh

--- controller/login.js
+++ controller/login.js
@@ -3,3 +3,7 @@ const User = require("../models/user");
 exports.loginController = async (req, res) => {
   res.render("login");
 };
+
+exports.googleLoginController = async (req, res) => {
+  res.send("login with google");
+};
--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
-const { loginController } = require("../controller/login");
+const {
+  loginController,
+  googleLoginController,
+} = require("../controller/login");
 
 router.route("/login").get(loginController);
+router.route("/google").get(googleLoginController);
 
 module.exports = router;
